c8476dcf77bf36b8e879f8f079569852dba8090c
Devender Kumar <145530874+DK420786@users.noreply.github.com>
2026-08-28 20:04:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return open("index.html", encoding="utf-8").read()
 
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form.get("name")
+    phone = request.form.get("phone")
+    email = request.form.get("email")
+    date = request.form.get("date")
+    service = request.form.get("service")
 
-@app.route("/appointment")
-def appointment():
-    return render_template("index.html")
+    print("New Booking:")
+    print("Name:", name)
+    print("Phone:", phone)
+    print("Email:", email)
+    print("Date:", date)
+    print("Service:", service)
 
+    return jsonify({
+        "success": True,
+        "message": "Request submitted successfully"
+    })
 
 if __name__ == "__main__":
     app.run(debug=True)
